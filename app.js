a3c78ba5c67950b25d246818647f7a790757f5ae
teymitee1 <teymitee@gmail.com>
2019-03-06 18:19:46 +0100

    formatted code

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-var express             = require("express"),
-    app                 = express(),
-    bodyParser          = require("body-parser"),
-    mongoose            = require("mongoose"),
-    passport            = require("passport"),
-    LocalStrategy       = require("passport-local"),
-    methodOverride      = require("method-override"),
-    Campground          = require("./models/campground"),
-    Comment             = require("./models/comments"),
-    User                = require("./models/user"),
-    seedDB              = require("./seeds");
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
+    Campground = require("./models/campground"),
+    Comment = require("./models/comments"),
+    User = require("./models/user"),
+    seedDB = require("./seeds");
 
 //requiring routes
-var commentRoutes       = require("./routes/comments"),
-    campgroundRoutes    = require("./routes/campgrounds"),
-    indexRoutes         = require("./routes/index");
+var commentRoutes = require("./routes/comments"),
+    campgroundRoutes = require("./routes/campgrounds"),
+    indexRoutes = require("./routes/index");
 
 mongoose.connect("mongodb://localhost/yelpcamp");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
@@ -36,9 +36,9 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     res.locals.currentUser = req.user;
-    next ();
+    next();
 });
 
 
@@ -46,6 +46,6 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, "127.0.0.1", ()=>{
+app.listen(3000, "127.0.0.1", () => {
     console.log("The YelpCamp Server Has Started");
 });
--- seeds.js
+++ seeds.js
@@ -3,51 +3,51 @@ var Campground = require("./models/campground");
 var Comment = require("./models/comments");
 
 
-    var data = [
-        {
-            name: "You Came Back From Work & Found This Strange Woman In Your Kitchen, What Will You Do?",
-            image: "http://www.naijaloaded.com.ng/wp-content/uploads/2018/11/Big-woman.jpg",
-            description: "Health and safety procedures must be followed by the employee during the training and induction day. This can be achieved by going through the manufacturers instruction on how the material will be moved and stored. If any problem arises, this can be done by informing the site supervisor and wait till the problem is solved."
-        },
-        {
-            name: "Question Of The Day:- If Buhari Is A Folder On Your Computer, What Would You Be Saving Inside?",
-            image: "http://www.naijaloaded.com.ng/wp-content/uploads/2018/11/67890987940.jpg",
-            description: "Health and safety procedures must be followed by the employee during the training and induction day. This can be achieved by going through the manufacturers instruction on how the material will be moved and stored. If any problem arises, this can be done by informing the site supervisor and wait till the problem is solved."
-        },
-        {
-            name: "REAL LIFE TALK!! Has Anything Ever Happened To You That Made You Say, “Ha, I Must Get This Money O”?",
-            image: "http://www.naijaloaded.com.ng/wp-content/uploads/2018/11/header-20.jpg",
-            description: "Health and safety procedures must be followed by the employee during the training and induction day. This can be achieved by going through the manufacturers instruction on how the material will be moved and stored. If any problem arises, this can be done by informing the site supervisor and wait till the problem is solved."
-        }
-    ];
+var data = [
+    {
+        name: "You Came Back From Work & Found This Strange Woman In Your Kitchen, What Will You Do?",
+        image: "http://www.naijaloaded.com.ng/wp-content/uploads/2018/11/Big-woman.jpg",
+        description: "Health and safety procedures must be followed by the employee during the training and induction day. This can be achieved by going through the manufacturers instruction on how the material will be moved and stored. If any problem arises, this can be done by informing the site supervisor and wait till the problem is solved."
+    },
+    {
+        name: "Question Of The Day:- If Buhari Is A Folder On Your Computer, What Would You Be Saving Inside?",
+        image: "http://www.naijaloaded.com.ng/wp-content/uploads/2018/11/67890987940.jpg",
+        description: "Health and safety procedures must be followed by the employee during the training and induction day. This can be achieved by going through the manufacturers instruction on how the material will be moved and stored. If any problem arises, this can be done by informing the site supervisor and wait till the problem is solved."
+    },
+    {
+        name: "REAL LIFE TALK!! Has Anything Ever Happened To You That Made You Say, “Ha, I Must Get This Money O”?",
+        image: "http://www.naijaloaded.com.ng/wp-content/uploads/2018/11/header-20.jpg",
+        description: "Health and safety procedures must be followed by the employee during the training and induction day. This can be achieved by going through the manufacturers instruction on how the material will be moved and stored. If any problem arises, this can be done by informing the site supervisor and wait till the problem is solved."
+    }
+];
 
-function seedDB(){
-    Campground.remove({}, (err)=>{
-    //     if(err){
-    //         console.log(err);
-    //     }
-    //     console.log("All Campgrounds Removed");
-    // });
-    // data.forEach((seed)=>{
-    //     Campground.create(seed, (err, campground)=>{
-    //         if(err){
-    //             console.log(err);
-    //         }else {
-    //             console.log("campground created");
-    //             Comment.create({
-    //                 text: "This place is cool, but i wish it had internet",
-    //                 author: "Temitope"
-    //             }, (err, comment)=>{
-    //                 if(err){
-    //                     console.log(err);
-    //                 }else {
-    //                     campground.comments.push(comment);
-    //                     campground.save();
-    //                     console.log("Comments created")
-    //                 }
-    //             });
-    //         }
-    //     });
+function seedDB() {
+    Campground.remove({}, (err) => {
+        //     if(err){
+        //         console.log(err);
+        //     }
+        //     console.log("All Campgrounds Removed");
+        // });
+        // data.forEach((seed)=>{
+        //     Campground.create(seed, (err, campground)=>{
+        //         if(err){
+        //             console.log(err);
+        //         }else {
+        //             console.log("campground created");
+        //             Comment.create({
+        //                 text: "This place is cool, but i wish it had internet",
+        //                 author: "Temitope"
+        //             }, (err, comment)=>{
+        //                 if(err){
+        //                     console.log(err);
+        //                 }else {
+        //                     campground.comments.push(comment);
+        //                     campground.save();
+        //                     console.log("Comments created")
+        //                 }
+        //             });
+        //         }
+        //     });
     });
 
 }
