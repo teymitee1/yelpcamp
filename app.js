6270c6f68bef8dd69bf6e6b0f556dcdf4cb5bfec
teymitee1 <teymitee@gmail.com>
2019-03-11 22:49:13 +0100

    debugging

--- app.js
+++ app.js
@@ -54,6 +54,4 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, "127.0.0.1", () => {
-    console.log("The YelpCamp Server Has Started");
-});
+app.listen(process.env.PORT);
