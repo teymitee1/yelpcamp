09c0f61ad796aca3ba549511fecb654efd62e841
teymitee1 <teymitee@gmail.com>
2019-03-13 16:32:20 +0100

    deploy

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express"),
   app = express(),
   bodyParser = require("body-parser"),
@@ -17,30 +16,8 @@ let commentRoutes = require("./routes/comments"),
   campgroundRoutes = require("./routes/campgrounds"),
   indexRoutes = require("./routes/index");
 
-var url = process.env.DATABASEURL || "mongodb+srv://teymitee:4KdHUEJmd8w21t5h@yelpcamp-amcv1.mongodb.net/YelpCamp?retryWrites=true";
+var url = process.env.DATABASEURL || "mongodb://localhost/yelpcamp";
 mongoose.connect(url, { useNewUrlParser: true });
-=======
-const   express = require("express"),
-        app = express(),
-        bodyParser = require("body-parser"),
-        mongoose = require("mongoose"),
-        flash = require("connect-flash"),
-        passport = require("passport"),
-        LocalStrategy = require("passport-local"),
-        methodOverride = require("method-override"),
-        Campground = require("./models/campground"),
-        Comment = require("./models/comments"),
-        User = require("./models/user"),
-        seedDB = require("./seeds");
-
-//requiring routes
-let     commentRoutes = require("./routes/comments"),
-        campgroundRoutes = require("./routes/campgrounds"),
-        indexRoutes = require("./routes/index");
-
-// mongoose.connect("mongodb://localhost/yelpcamp", {useNewUrlParser: true});
-mongoose.connect("mongodb://teymitee:%40Conjugate1@cluster0-shard-00-00-amcv1.mongodb.net:27017,cluster0-shard-00-01-amcv1.mongodb.net:27017,cluster0-shard-00-02-amcv1.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true");
->>>>>>> origin/master
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
@@ -49,17 +26,10 @@ app.use(methodOverride("_method"));
 app.use(flash());
 
 //PASSPORT CONFIG
-<<<<<<< HEAD
 app.use(require("cookie-session")({
   secret: "Temitope created this application",
   resave: false,
   saveUninitialized: false
-=======
-app.use(require("express-session")({
-    secret: "Temitope created this application",
-    resave: false,
-    saveUninitialized: false
->>>>>>> origin/master
 }));
 
 app.use(passport.initialize());
@@ -69,17 +39,10 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-<<<<<<< HEAD
   res.locals.currentUser = req.user;
   res.locals.error = req.flash("error");
   res.locals.success = req.flash("success");
   next();
-=======
-    res.locals.currentUser = req.user;
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
->>>>>>> origin/master
 });
 
 
@@ -87,15 +50,9 @@ app.use(indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-<<<<<<< HEAD
-const host = '0.0.0.0';
+const host = process.env.IP || '0.0.0.0';
 const port = process.env.PORT || 3000;
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(port, host, function(){
   console.log("YelpCamp is running");
 })
-=======
-app.listen(3000, "127.0.0.1", () => {
-    console.log("The YelpCamp Server Has Started");
-});
->>>>>>> origin/master
